3b429bd0698cbc5bd762d1db17d58eac19ccce2f
singaparado <singaparadom@gmail.com>
2026-06-21 05:13:55 +0800

    Update transform.mjs

--- api/transform.mjs
+++ api/transform.mjs
@@ -34,8 +34,6 @@ export default async function handler(req, res) {
         body: JSON.stringify({
 
           model: "claude-3-5-sonnet-20240620",
-,
-
           max_tokens: 1000,
 
           system:
