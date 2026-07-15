67b675f554a68e392bcb2200223ad22bebe97400
singaparado <singaparadom@gmail.com>
2026-07-15 13:15:14 +0800

    Delete legibility-score directory

--- legibility-score/src/component/diagnostic/index.ts
+++
@@ -1,4 +0,0 @@
-export { DiagnosticPanel } from "./DiagnosticPanel";
-export { ScoreDisplay } from "./ScoreDisplay";
-export { WindowCard } from "./WindowCard";
-export { LawCard } from "./LawCard";
